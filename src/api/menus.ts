5dd0f9ede9ab917a5358b579e04dd81d373bda16
Maël GUERIN <mael.guerin@murena.io>
2025-01-11 18:05:46 +0100

    feat: add `getMenuDishes` api function

--- src/api/menus.ts
+++ src/api/menus.ts
@@ -25,3 +25,59 @@ export const getMenus = async (session: Session, establishment: Establishment) =
 
   return menus;
 };
+
+export const getMenuDishes = async (session: Session, menu: Menu, date: Date) => {
+  const menuPageRequest = new Request(menu.url + "/" + date.getFullYear() + "/" + (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getDate().toString().padStart(2, "0") );
+  menuPageRequest.setSession(session);
+
+  const menuPageResponse = await menuPageRequest.send(session.fetcher);
+
+  const document = parse(menuPageResponse.content);
+
+  const dishContainers = document.querySelectorAll(".menu_composante_container");
+
+  const entry: string[] = [];
+  const side: string[] = [];
+  const main: string[] = [];
+  const dairy: string[] = [];
+  const dessert: string[] = [];
+
+  for (const container of dishContainers) {
+    const titleEl = container.querySelector(".menu_composante_title");
+    const title = titleEl!.textContent.split("-")[1].trim();
+
+    const dishesEl = container.querySelectorAll(".menu-composante-libelle");
+
+    const dishes: string[] = [];
+
+    for (const dishEl of dishesEl) {
+      dishes.push(dishEl.textContent.trim());
+    }
+
+    switch (title) {
+      case "Hors d'oeuvre":
+        entry.push(...dishes);
+        break;
+      case "Plat":
+        main.push(...dishes);
+        break;
+      case "Garniture":
+        side.push(...dishes);
+        break;
+      case "Produit Laitier":
+        dairy.push(...dishes);
+        break;
+      case "Dessert":
+        dessert.push(...dishes);
+        break;
+    }
+  }
+
+  return {
+    entry,
+    side,
+    main,
+    dairy,
+    dessert
+  };
+};
--- src/models/menu.ts
+++ src/models/menu.ts
@@ -1,3 +1,11 @@
+export type Dishes = Readonly<{
+  entry: string[],
+  side: string[],
+  main: string[],
+  dairy: string[],
+  dessert: string[]
+}>;
+
 export type Menu = Readonly<{
   name: string,
   url: string
