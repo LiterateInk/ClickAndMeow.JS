57ce59d82d9c132bd1b881dfb6a89efd95ac1276
Maël GUERIN <mael.guerin@murena.io>
2025-01-11 17:08:53 +0100

    refactor: lint

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "tsup";
 export default defineConfig({
-    entry: ["src/index.ts"],
-    outDir: "dist",
-    format: ["cjs", "esm"],
-    treeshake: true,
-    splitting: false,
-    sourcemap: true,
-    minify: "terser",
-    clean: true,
-    dts: true
+  entry: ["src/index.ts"],
+  outDir: "dist",
+  format: ["cjs", "esm"],
+  treeshake: true,
+  splitting: false,
+  sourcemap: true,
+  minify: "terser",
+  clean: true,
+  dts: true
 });
